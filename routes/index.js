5ff87ad585ae30babc2f5405140d13182a685b35
Alba C <albacabadotorrado@gmail.com>
2023-02-01 13:58:24 +0100

    Endpoints funcionales

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('holaaa');
 });
 
+router.get('/employees', function(req, res, next) {
+  res.send('obteniendo empleados');
+});
+router.post('/employees', function(req, res, next) {
+  res.send('creando empleados');
+});
+router.put('/employees', function(req, res, next) {
+  res.send('actualizando empleado');
+});
+router.delete('/employees', function(req, res, next) {
+  res.send('eliminando empleado');
+});
 module.exports = router;
